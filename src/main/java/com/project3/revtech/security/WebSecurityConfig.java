1c9a223fc4d851807366608b55789c6675b0e509
smei8 <94879898+smei8@users.noreply.github.com>
2022-04-12 12:29:42 -0400

    updated wishlist endpoints

--- src/main/java/com/project3/revtech/security/WebSecurityConfig.java
+++ src/main/java/com/project3/revtech/security/WebSecurityConfig.java
@@ -23,99 +23,93 @@ import com.project3.revtech.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-		prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  @Autowired
-  UserDetailsServiceImpl userDetailsService;
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
 
-  @Autowired
-  private AuthEntryPointJwt unauthorizedHandler;
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
 
-  @Bean
-  public AuthTokenFilter authenticationJwtTokenFilter() {
-    return new AuthTokenFilter();
-  }
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	}
 
-  @Override
-  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-  }
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
 
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-  @Bean
-  ServletRegistrationBean h2servletRegistration() {
-      ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-      registrationBean.addUrlMappings("/h2-console/*");
-      return registrationBean;
-  }
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-	   http.cors().and().csrf().disable()
-	  .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-	  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-	   and().authorizeRequests().antMatchers("/api/auth/**").permitAll().and()
-	  .authorizeRequests().antMatchers("/h2-console/**").permitAll().and()
-      .authorizeRequests().antMatchers("/api/product-discount").permitAll().and()
-      .authorizeRequests().antMatchers("/api/product-discount/**").permitAll().and()
-      .authorizeRequests().antMatchers("/api/products").permitAll().and()
-      .authorizeRequests().antMatchers("/api/products/**").permitAll().and()
-      .authorizeRequests().antMatchers("/api/auth/signup").permitAll().and()
-      .authorizeRequests().antMatchers("/combined/Disc/Products").permitAll().and()
-	  .authorizeRequests().antMatchers("/api/products/{pid}").permitAll().and()
-	  //.authorizeRequests().antMatchers("/file/upload").permitAll().and()
-      //.authorizeRequests().antMatchers("/discount/update/discounts/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/discount/remove/discounts/{discId}/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/discount/add/discounts/**").permitAll().and()
-      .authorizeRequests().antMatchers("/discount/all/discounts/**").permitAll().and()
-      .authorizeRequests().antMatchers("/discount/all/discountedProducts/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/transaction").permitAll().and()
-       //.authorizeRequests().antMatchers("/api/transaction/**").permitAll().and()
-       //.authorizeRequests().antMatchers("/api/purchased-items/**").permitAll().and()
-       .authorizeRequests().antMatchers("/api/reviews/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/cart-items").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/cart-items/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/file").permitAll().and()
-      //.authorizeRequests().antMatchers("/file/**").permitAll().and()
-      .authorizeRequests().antMatchers("/api/reset_password/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/cart-and-items/**").permitAll().and()
-       //.authorizeRequests().antMatchers("/api/cart").permitAll().and()
-       //.authorizeRequests().antMatchers("/api/cart/**").permitAll().and()
-      .authorizeRequests().antMatchers("/combined/Disc/Products").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/test/**").permitAll().and()
-      //.authorizeRequests().antMatchers("/cart/{bid}").permitAll().and()
+	@Bean
+	ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/h2-console/*");
+		return registrationBean;
+	}
 
-      //.authorizeRequests().antMatchers("/api/create/bundles").permitAll().and()
-      .authorizeRequests().antMatchers("/user/{bid}").permitAll().and()
-      .authorizeRequests().antMatchers("/api/bundles").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/wishList/add/wishLists").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/wishList/user/{bid}/get").permitAll().and()
-      //.authorizeRequests().antMatchers("/api/wishList-items/add/items").permitAll().and()
-      .authorizeRequests().antMatchers("/api/wishList-items/{bid}/delete").permitAll().anyRequest().authenticated();
-	   http.headers().frameOptions().disable();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-		  
-	   
-	   http.addFilterBefore(authenticationJwtTokenFilter(),
-	   UsernamePasswordAuthenticationFilter.class);
-		 
-			/*
-			 * http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll
-			 * (); http.headers().frameOptions().disable();
-			 */
-    
-  }
- 
-  
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/api/auth/**").permitAll().and().authorizeRequests().antMatchers("/h2-console/**")
+				.permitAll().and().authorizeRequests().antMatchers("/api/product-discount").permitAll().and()
+				.authorizeRequests().antMatchers("/api/product-discount/**").permitAll().and().authorizeRequests()
+				.antMatchers("/api/products").permitAll().and().authorizeRequests().antMatchers("/api/products/**")
+				.permitAll().and().authorizeRequests().antMatchers("/api/auth/signup").permitAll().and()
+				.authorizeRequests().antMatchers("/combined/Disc/Products").permitAll().and().authorizeRequests()
+				.antMatchers("/api/products/{pid}").permitAll().and()
+				// .authorizeRequests().antMatchers("/file/upload").permitAll().and()
+				// .authorizeRequests().antMatchers("/discount/update/discounts/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/discount/remove/discounts/{discId}/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/discount/add/discounts/**").permitAll().and()
+				.authorizeRequests().antMatchers("/discount/all/discounts/**").permitAll().and().authorizeRequests()
+				.antMatchers("/discount/all/discountedProducts/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/transaction").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/transaction/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/purchased-items/**").permitAll().and()
+				.authorizeRequests().antMatchers("/api/reviews/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/cart-items").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/cart-items/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/file").permitAll().and()
+				// .authorizeRequests().antMatchers("/file/**").permitAll().and()
+				.authorizeRequests().antMatchers("/api/reset_password/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/cart-and-items/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/cart").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/cart/**").permitAll().and()
+				.authorizeRequests().antMatchers("/combined/Disc/Products").permitAll().and()
+				// .authorizeRequests().antMatchers("/api/test/**").permitAll().and()
+				// .authorizeRequests().antMatchers("/cart/{bid}").permitAll().and()
+
+				// .authorizeRequests().antMatchers("/api/create/bundles").permitAll().and()
+				.authorizeRequests().antMatchers("/user/{bid}").permitAll().and().authorizeRequests()
+				.antMatchers("/api/bundles").permitAll().and()
+
+				.authorizeRequests().antMatchers("/api/wishList/add/wishLists").permitAll().and().authorizeRequests()
+				.antMatchers("/api/wishList/user/{bid}/get").permitAll().and().authorizeRequests()
+				.antMatchers("/api/wishList-items/add/items").permitAll().and().authorizeRequests()
+				.antMatchers("/api/wishList-items/{bid}/delete").permitAll().anyRequest().authenticated();
+		http.headers().frameOptions().disable();
+
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		/*
+		 * http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll
+		 * (); http.headers().frameOptions().disable();
+		 */
+
+	}
 
 }
